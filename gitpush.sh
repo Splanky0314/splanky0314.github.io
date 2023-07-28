30121ba3ed373134fd4be0f69ea67610043a6990
splanky0314 <splanky0314@gmail.com>
2023-07-28 19:17:06 +0900

    2023-07-28-Upload

--- gitpush.sh
+++ gitpush.sh
@@ -6,10 +6,11 @@ echo "git add ."
 
 git add .
 
-echo "git commit" >> $(date +'%Y-%m-%d %H:%M:%S %Z')
+echo "git commit" 
+echo $(date +'%Y-%m-%d %H:%M:%S %Z')
 
-# git commit -m $(date +'%Y-%m-%d %H:%M:%S %Z') 
+git commit -m $(date +'%Y-%m-%d--%H:%M:%S %Z') 
 
-# echo "git push"
+echo "git push"
 
-# git push
+git push
